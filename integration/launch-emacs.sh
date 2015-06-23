7daea6f9cf10ec9cba9b96d9238b9048b0054db6
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-06-23 11:14:25 +0300

    focus client

--- integration/launch-emacs.sh
+++ integration/launch-emacs.sh
@@ -8,7 +8,7 @@ function find_window {
 find_window
 if [ $? != 0 ];
 then
-    emacsclient -c -a ""
+    emacsclient -c -a "" -e "(select-frame-set-input-focus (selected-frame))"
 else
     set -- $windows
     if [ $1 ]; then
