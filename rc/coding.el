1ff84378aa566793d1019f728a4af6fc8bf9563f
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-07-28 12:04:43 +0300

    indent json by two spaces

--- rc/coding.el
+++ rc/coding.el
@@ -228,7 +228,12 @@
   (setq-default coffee-tab-width 2))
 
 (use-package json-mode
-  :ensure t)
+  :ensure t
+  :config
+  (add-hook 'json-mode-hook
+            (lambda ()
+              (make-local-variable 'js-indent-level)
+              (setq js-indent-level 2))))
 
 (use-package js2-mode
   :ensure t
