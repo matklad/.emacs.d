c7ed40b92e5a042f995f87531cc67e3329ad2dad
Alex Kladov <aleksey.kladov@gmail.com>
2026-08-09 18:27:46 +0100

    .

--- init.el
+++ init.el
@@ -7,11 +7,14 @@
       mac-command-modifier 'meta
       mac-option-modifier 'super
       scroll-error-top-bottom t
+      completion-styles '(flex)
       )
 
 (setq-default indent-tabs-mode nil
 	          tab-width 4
 	          fill-column 80)
+
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-16"))
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -23,6 +26,7 @@
 
 (load-theme 'leuven t)
 
+(add-hook 'compilation-filter-hook #'ansi-color-compilation-filter)
 
 (global-set-key (kbd "M-z") #'undo)
 (global-set-key (kbd "M-c") #'copy-region-as-kill)
@@ -50,7 +54,7 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(dolist (pkg '(zig-mode magit multiple-cursors devil magit))
+(dolist (pkg '(zig-mode magit multiple-cursors devil magit zenburn-theme))
   (unless (package-installed-p pkg)
     (package-install pkg)))
 
@@ -58,8 +62,9 @@
 (global-set-key (kbd "M-x") #'kill-region)
 (global-set-key (kbd "C-c x") #'execute-extended-command)
 
-(setq devil-translations
-      '((", x" . "C-c x")))
+(setq devil-translations '((", x" . "C-c x")
+                           (", p" . "C-x p")
+                           (", b" . "C-x")))
 (global-devil-mode 1)
 
 (defun visit-init-file ()
@@ -72,7 +77,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(devil magit multiple-cursors zig-mode)))
+ '(custom-safe-themes
+   '("745f8c882e6edae45476e93f7b47c5bd4a4dc98c65494672ddcd291359935a3a" default))
+ '(package-selected-packages
+   '(devil magit multiple-cursors zenburn zenburn-theme zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
