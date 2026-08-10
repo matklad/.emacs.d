23af4fb5bc54101e947c525933e26dad03fb09b3
Alex Kladov <aleksey.kladov@gmail.com>
2026-08-10 13:52:07 +0100

    changes

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
       mac-option-modifier 'super
       scroll-error-top-bottom t
       completion-styles '(flex)
+      auto-save-visited-interval 1
+      after-focus-change-function (lambda () (save-some-buffers t))
       )
 
 (setq-default indent-tabs-mode nil
@@ -23,6 +25,7 @@
 (electric-pair-mode 1)
 (recentf-mode 1)
 (global-hl-line-mode 1)
+(auto-save-visited-mode 1)
 
 (load-theme 'leuven t)
 
@@ -31,6 +34,8 @@
 (global-set-key (kbd "M-z") #'undo)
 (global-set-key (kbd "M-c") #'copy-region-as-kill)
 (global-set-key (kbd "M-v") #'yank)
+(global-set-key (kbd "M-x") #'kill-region)
+(define-key key-translation-map (kbd "ESC") (kbd "C-g"))
 
 (global-set-key (kbd "M-<left>")  #'beginning-of-line)
 (global-set-key (kbd "M-<right>") #'end-of-line)
@@ -54,18 +59,40 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(dolist (pkg '(zig-mode magit multiple-cursors devil magit zenburn-theme))
+(dolist (pkg '(zig-mode magit multiple-cursors devil magit zenburn-theme
+                        hydra vertico orderless))
   (unless (package-installed-p pkg)
     (package-install pkg)))
+
+(require 'vertico)
+(vertico-mode)
+
+(require 'orderless)
+(setq completion-styles '(orderless basic))
 
 (require 'devil)
 (global-set-key (kbd "M-x") #'kill-region)
 (global-set-key (kbd "C-c x") #'execute-extended-command)
 
-(setq devil-translations '((", x" . "C-c x")
-                           (", p" . "C-x p")
-                           (", b" . "C-x")))
+(add-to-list 'devil-translations '(", m x" . "C-c x"))
 (global-devil-mode 1)
+
+(require 'hydra)
+
+(defhydra project-hydra (:color teal)
+  ("f" project-find-file "find file")
+  ("g" project-find-regexp "find regexp")
+  ("d" project-dired "dired")
+  ("b" project-switch-to-buffer "switch buffer")
+  ("p" project-switch-project "switch project")
+  ("k" project-kill-buffers "kill buffers")
+  ("c" project-compile "compile")
+  ("e" project-eshell "eshell")
+  ("v" project-vc-dir "vc dir")
+  ("x" project-shell-command "shell command")
+  ("X" project-async-shell-command "async shell command"))
+
+(global-set-key (kbd "C-x p") #'project-hydra/body)
 
 (defun visit-init-file ()
   "Open the user's Emacs init file."
@@ -80,7 +107,7 @@
  '(custom-safe-themes
    '("745f8c882e6edae45476e93f7b47c5bd4a4dc98c65494672ddcd291359935a3a" default))
  '(package-selected-packages
-   '(devil magit multiple-cursors zenburn zenburn-theme zig-mode)))
+   '(devil hydra magit multiple-cursors orderless vertico zenburn-theme zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
